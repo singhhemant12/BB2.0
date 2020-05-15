fdba63f3a3e7c58bc81e031b930cbc5f9bddbe2b
682hemant <premprataps1998@gmail.com>
2020-05-15 18:55:11 +0530

    modified files

--- src/assets/script/index.js
+++ src/assets/script/index.js
@@ -1,9 +1,9 @@
 
-$(".nav-link").click(function(){
-    $(".nav-link").css({"background-color":"" ,"border-radius":"","transform":""});
-    $(this).css({"background-color":"rgba(0, 87, 255, 0.1)","border-radius":"18px"});
-    $(this).find('span i.fas').css({"transform":"rotate(180deg)"});
-});
+// $(".nav-link").click(function(){
+//     $(".nav-link").css({"background-color":"" ,"border-radius":"","transform":""});
+//     $(this).css({"background-color":"rgba(0, 87, 255, 0.1)","border-radius":"18px"});
+//     $(this).find('span i.fas').css({"transform":"rotate(180deg)"});
+// });
 
 // $("#navbarDropdown").click(function () {
 //     $(this).find('span i.fas').toggleClass('fa-angle-up  fa-angle-down ');
